0bdeed1ea200938958fb1ddd637f6c80339dc82a
Rehman <139582006+REHMAN210@users.noreply.github.com>
2023-08-11 16:06:21 +0530

    Add files via upload

---
+++ reh4.js
@@ -0,0 +1,11 @@
+function myAwesomeFunction(myString, myString2 = 0) {
+
+    console.log(myString);
+    console.log(myString2);
+
+    return undefined;
+
+}
+let myResult = myAwesomeFunction("abc","def");
+
+console.log(myResult);
